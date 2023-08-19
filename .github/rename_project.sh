7efcba7c8184831e5347630087724ac8cc63f456
gdecker1 <gdecker1@users.noreply.github.com>
2023-08-19 06:21:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="gdecker1"
+original_name="graphalith"
+original_urlname="Graphalith"
+original_description="Awesome graphalith created by gdecker1"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv graphalith $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ graphalith/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for graphalith."""
+
+from graphalith.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
